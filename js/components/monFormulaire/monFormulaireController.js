8558d33a8cf7017119ce38fb4a5aae70a7b1fd6f
Elsa Delgado <elsadd@hotmail.fr>
2017-02-25 23:52:55 +0100

    essais CSS + controls

--- js/components/monFormulaire/monFormulaireController.js
+++ js/components/monFormulaire/monFormulaireController.js
@@ -1,29 +1,31 @@
-angular.module('monFormulaire').controller('monFormulaireController', [function(){
-	let monFormulaireCollection;
-	this.controlName ;
-	this.controlAge;
-	this.controlAdress;
-	this.controlEmail;
+angular.module('monFormulaire').controller('monFormulaireController', [function($scope) {
+		let monFormulaireCollection;
+		this.controlName = /\b[A-Za-z][a-z\-]{1,50}/;
+		this.controlAge = /^\d{1,3}/;
+		this.controlAdress = /[\b0-9]{0,3} [a-zA-Z ,]{5,30}\b[0-9]{5}\b \b[A-Z][a-z\-]{1,50}/;
+		this.controlEmail;
 
-	this.firstname = "name"
-	
-}])
-
-
-
-// angular.module("monFormulaire").controller('monFormulaireController', ['$http', function monFormulaireController($http){
-// 	let monFormulaireCollection;
-// 	this.controlName ;
-// 	this.controlAge;
-// 	this.controlAdress;
-// 	this.controlEmail;
-
-// 	this.add = () => {
-// 		if (this.student.lastname) {
-// 			this.students.push(new Student(this.student))
-// 		} else {
-// 			alert("lastname can't be empty")
-// 		}
-// 	}
-
-// }])
+		
+		$scope.test = function() {
+			$scope.incomplete = false;
+			if ($scope.myBtn && (!$scope.firstname.length ||
+					!$scope.lastname.length ||
+					!$scope.Age.length || !$scope.address.length)) {
+				$scope.incomplete = true;
+			}
+		};
+	}])
+	// angular.module("monFormulaire").controller('monFormulaireController', ['$http', function monFormulaireController($http){
+	// 	let monFormulaireCollection;
+	// 	this.controlName ;
+	// 	this.controlAge;
+	// 	this.controlAdress;
+	// 	this.controlEmail;
+	// 	this.add = () => {
+	// 		if (this.student.lastname) {
+	// 			this.students.push(new Student(this.student))
+	// 		} else {
+	// 			alert("lastname can't be empty")
+	// 		}
+	// 	}
+	// }])
