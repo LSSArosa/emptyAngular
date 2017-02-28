f9562f22addf514a33c2a81a2fca6bf15d76ed26
Elsa Delgado <elsadd@hotmail.fr>
2017-02-28 17:00:59 +0100

    insertion tableau ok, Todo reset du form entre 2validations

--- js/components/monFormulaire/monFormulaireComponent.js
+++ js/components/monFormulaire/monFormulaireComponent.js
@@ -1,8 +1,32 @@
 'use strict';
 angular.module("monFormulaire").component("monFormulaire", {
 	templateUrl: "js/components/monFormulaire/monFormulaireTemplate.html",
-	controller: "monFormulaireController"
-}) 
+	// controller: "monFormulaireController"
+	controller: function monFormulaireController() {
+		
+		this.controlName = /\b[A-Za-z][a-z\-]{1,50}/;
+		this.controlAge = /^\d{1,3}/;
+		this.controlAddress = /[\b0-9]{0,3} [a-zA-Z ,]{5,30}\b[0-9]{5}\b \b[A-Z][a-z\-]{1,50}/;
+		this.controlEmail;
+
+		this.rowCollection = [];
+		this.addRow =() =>{
+			let newRow = {
+				"firstname": this.firstname,
+				"lastname": this.lastname,
+				"age": this.age,
+				"address": this.address,
+				"email": this.email
+			};
+			this.rowCollection.push(newRow);
+			this.studentForm.setPristine();
+		}
+		// this.reset = () => {
+  //   		this.studentForm = undefined
+  // 		}
+
+	}
+});
 
 
 
--- js/components/monFormulaire/monFormulaireController.js
+++ js/components/monFormulaire/monFormulaireController.js
@@ -1,20 +1,14 @@
-angular.module('monFormulaire').controller('monFormulaireController', [function($scope) {
-		let monFormulaireCollection;
-		this.controlName = /\b[A-Za-z][a-z\-]{1,50}/;
-		this.controlAge = /^\d{1,3}/;
-		this.controlAdress = /[\b0-9]{0,3} [a-zA-Z ,]{5,30}\b[0-9]{5}\b \b[A-Z][a-z\-]{1,50}/;
-		this.controlEmail;
 
-		
-		$scope.test = function() {
-			$scope.incomplete = false;
-			if ($scope.myBtn && (!$scope.firstname.length ||
-					!$scope.lastname.length ||
-					!$scope.Age.length || !$scope.address.length)) {
-				$scope.incomplete = true;
-			}
-		};
-	}])
+
+				// $scope.test = function() {
+		// 	$scope.incomplete = false;
+		// 	if ($scope.myBtn && (!$scope.firstname.length ||
+		// 			!$scope.lastname.length ||
+		// 			!$scope.Age.length || !$scope.address.length)) {
+		// 		$scope.incomplete = true;
+		// 	}
+		// };
+
 	// angular.module("monFormulaire").controller('monFormulaireController', ['$http', function monFormulaireController($http){
 	// 	let monFormulaireCollection;
 	// 	this.controlName ;
